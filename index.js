3ac0979a3b5fbac30a1c5678db065f47a5d5930a
prashant-80 <prashantsingh200502@gmail.com>
2024-06-24 15:04:13 +0530

    env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config();
 
 app.get('/home',(req,res)=>{
     res.json({message:'OK'})
 })
-app.listen(3001,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('started a server')
 })
